9bd3eaeed704de63bd9ef3fa3ae8866e03ef81d5
Aravin008 <>
2026-02-28 00:02:23 +0530

    Added UI fixes and flow fixes

--- src/components/customNodes/ConditionNode.vue
+++ src/components/customNodes/ConditionNode.vue
@@ -3,8 +3,8 @@ import { Handle, Position } from '@vue-flow/core'
 </script>
 
 <template>
-  <div class="bg-yellow-100 border rounded p-2 min-w-37.5">
-    <div class="text-center"><strong>Condition</strong></div>
+  <div class="relative bg-yellow-100 border rounded p-2 min-w-37.5">
+    <div class="text-center pb-1"><strong>Condition</strong></div>
 
     <!-- Incoming -->
     <Handle
@@ -13,6 +13,9 @@ import { Handle, Position } from '@vue-flow/core'
     />
 
     <!-- True branch -->
+    <div class="absolute -left-1.25 -rotate-z-90 top-1/2 -translate-y-1/2 text-xs text-green-700 pointer-events-none">
+      True
+    </div>
     <Handle
       id="left"
       type="source"
@@ -20,6 +23,9 @@ import { Handle, Position } from '@vue-flow/core'
     />
 
     <!-- False branch -->
+    <div class="absolute bottom-0 left-1/2 -translate-x-1/2 text-xs text-red-700 pointer-events-none">
+      False
+    </div>
     <Handle
       id="bottom"
       type="source"
--- src/components/editors/TransformNodeEditor.vue
+++ src/components/editors/TransformNodeEditor.vue
@@ -2,6 +2,7 @@
 import { useFlowStore } from '@/stores/flowStore'
 import type { Node } from '@vue-flow/core'
 import type { TransformNode, TypeTransformOp, TypeFieldType } from '@/types/nodes'
+import { computed } from 'vue';
 
 const props = defineProps<{
   node: Node<{ data: TransformNode }>
@@ -10,17 +11,38 @@ const props = defineProps<{
 const flow = useFlowStore()
 
 const fields: TypeFieldType[] = ['message', 'count']
-const operations: TypeTransformOp[] = [
-  'uppercase',
-  'prepend',
-  'append',
-  'add',
-  'multiply'
-]
+const operationsList = {
+  'message': [
+    'uppercase',
+    'prepend',
+    'append',
+  ],
+  'count': [
+    'add',
+    'multiply'
+  ]
+}
+
+function getDefaultOperation(field: TypeFieldType): TypeTransformOp {
+  return operationsList[field][0] as TypeTransformOp
+}
+
+const operations = computed(() => operationsList[props.node.data.field])
 
 function update(data: Partial<TransformNode>) {
   flow.updateNodeData(props.node.id, data)
 }
+
+function onFieldChange(event: Event) {
+  const field = (event.target as HTMLSelectElement).value as TypeFieldType
+
+  update({
+    field,
+    operation: getDefaultOperation(field),
+    value: ''
+  })
+}
+
 </script>
 
 <template>
@@ -30,7 +52,7 @@ function update(data: Partial<TransformNode>) {
       <select
         class="w-full border p-2 rounded"
         :value="props.node.data.field"
-        @change="update({ field: ($event.target as HTMLSelectElement).value as TypeFieldType })"
+        @change="onFieldChange"
       >
         <option v-for="f in fields" :key="f" :value="f">
           {{ f }}
